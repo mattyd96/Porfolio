c0697db02388c1c84679670f2e1299e807150816
Matthew <26681440+mattyd96@users.noreply.github.com>
2021-12-13 18:47:22 +1030

    added contact and footer nav: fully responsive

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const primaryNav = document.querySelector(".primary-nav");
 const navToggle = document.querySelector(".mobile-nav-button");
 
-
-navToggle.addEventListener("click", () => {
+function toggleNav() {
     const visibility = primaryNav.getAttribute('data-visible');
+    navToggle.classList.toggle("active");
 
     if(visibility === "false") {
         primaryNav.setAttribute("data-visible", true);
     } else {
         primaryNav.setAttribute("data-visible", false);
     }
-});
+}
